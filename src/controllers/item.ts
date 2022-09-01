3cbb3383ae6f44afbf528d6a961f88a371f7cd0a
centinela1024tb <centinela1024tb@gmail.com>
2022-09-01 13:01:22 -0500

    [ADD] Se impelement un manejador de errores
    => una funcion handleHttp que captura el error y mustra un mensaje

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,33 +1,49 @@
 import {Request, Response} from 'express'
+import { handleHttp } from '../utils/error.hendle'
 
 // ! Obtener un item
 const getItem = (req:Request, res:Response) => {
   try {
     
   } catch (e) {
-    res.status(500);
-    res.send('ERROR_GET_ITEM');
+    handleHttp(res, 'ERROR_GET_ITEM')
   }
 }
 
 // ! Obtener una lista de items
 const getItems = (req:Request, res:Response) => {
-  
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_GET_ITEMS')
+  }
 }
 
 // ! Crear un item
 const postItem = (req:Request, res:Response) => {
-  
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_POST_ITEM')
+  }
 }
 
 // ! Actualizar un item
 const updateItem = (req:Request, res:Response) => {
-  
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_UPDATE_ITEM')
+  }
 }
 
 // ! Eliminar un item
 const deleteItem = (req:Request, res:Response) => {
-  
+  try {
+    
+  } catch (e) {
+    handleHttp(res, 'ERROR_DELETE_ITEM')
+  }
 }
 
 export {
