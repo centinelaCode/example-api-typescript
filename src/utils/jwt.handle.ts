00aeba8a8a4792b3ca0ea7db052dcf5e628447fd
centinela1024tb <centinela1024tb@gmail.com>
2022-09-08 12:18:44 -0500

    [ADD] Se crea la función para generar el JWT

---
+++ src/utils/jwt.handle.ts
@@ -0,0 +1,23 @@
+import { sign, verify } from 'jsonwebtoken';
+
+//obtenemos el jsw_secret de la svariables de entorno
+const jwtSecret = process.env.JWT_SECRET || "token.01010101";
+
+// el payload que s ele va pasar va ser el user id
+const generateToken = async (id: string) => {
+  const jwt = sign({ id }, jwtSecret, {
+    expiresIn: "2h"
+  });
+  return jwt;
+}
+
+
+
+
+const verifyToken = async () => {}
+
+
+export {
+  generateToken,
+  verifyToken,
+}
